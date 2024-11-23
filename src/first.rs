eca90c45eb5cb6353989507a64e534628c1b9dd6
sytjason <jasont1020508@gmail.com>
2024-11-23 19:01:55 +0800

    add first

--- src/first.rs
+++ src/first.rs
@@ -1,9 +1,49 @@
-#[derive(Debug)]
-pub enum List {
-    Empty,
-    Elem(i32, Box<List>),
+use std::mem;
+
+pub struct List {
+    head: Link,
 }
 
-pub fn print_list(_list: List) {
-    println!("{:?}", _list);
+enum Link {
+    Empty,
+    More(Box<Node>),
+}
+
+struct Node {
+    elem: i32,
+    next: Link,
+}
+
+impl List {
+    pub fn print_list(&self) {
+        let mut p = &self.head;
+        while let Link::More(ref node) = p {
+            print!("{} -> ", node.elem);
+            p = &node.next;
+        }
+        println!("null");
+    }
+
+    pub fn new() -> Self {
+        List { head: Link::Empty }
+    }
+    
+    pub fn push(&mut self, _elem: i32) {
+        let new_node = Box::new(Node {
+            elem: _elem,
+            next: mem::replace(&mut self.head, Link::Empty),
+        });
+
+        self.head = Link::More(new_node);
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        match mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => None,
+            Link::More(node) => {
+                self.head = node.next;
+                Some(node.elem)
+            }
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 mod first;
-use first::{List, print_list};
+use first::{List};
 fn main() {
-    let mylist: List = List::Elem(1, Box::new(List::Empty));
-    print_list(mylist);
+    let mut list = List::new();
+    list.push(2);
+    list.push(32);
+    list.push(33);
+    list.push(39);
+    list.pop();
+    list.print_list();
 }
